ec7ca646b9c3d88ad0acd7e070e9fab184027900
github-main-user <kolya.skripn@mail.ru>
2025-05-31 16:46:17 +0300

    feat: create and connect mailings app

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("clients/", include("clients.urls", namespace="clients")),
+    path("mailings/", include("mailings.urls", namespace="mailings")),
 ]
