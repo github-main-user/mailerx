9e935c74b63286d841570673b5931c98b0354524
github-main-user <kolya.skripn@mail.ru>
2025-05-31 17:19:26 +0300

    feat: create Message model

--- mailings/models.py
+++ mailings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    subject = models.CharField(max_length=255)
+    body = models.TextField()
+
+    class Meta:
+        ordering = ("subject",)
+
+    def __str__(self):
+        return self.subject
