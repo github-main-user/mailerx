c1723e9f2d9617a5b9089e362762fb96ba2db7e4
github-main-user <kolya.skripn@mail.ru>
2025-05-31 15:28:55 +0300

    chore: register Client model

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ["owner", "email", "full_name"]
+    list_filter = ["owner", "email"]
+    search_fields = ["full_name"]
