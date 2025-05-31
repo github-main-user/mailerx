075aec5b367c6eaebbe58883585bfb855a8b5cb3
github-main-user <kolya.skripn@mail.ru>
2025-05-31 15:00:59 +0300

    feat: add client list feature

--- clients/urls.py
+++ clients/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from .apps import ClientsConfig
+from .views import ClientListView
 
 app_name = ClientsConfig.name
 
-urlpatterns = []
+urlpatterns = [
+    path("", ClientListView.as_view(), name="client_list"),
+]
