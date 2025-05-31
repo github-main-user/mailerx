472415b301bdbae66aad22b9c8e349e976dcce37
github-main-user <kolya.skripn@mail.ru>
2025-05-31 18:01:55 +0300

    feat(mailers): add home

--- mailings/urls.py
+++ mailings/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from .apps import MailingsConfig
+from .views import HomeView
 
 app_name = MailingsConfig.name
 
-urlpatterns = []
+urlpatterns = [
+    path("", HomeView.as_view(), name="home"),
+]
