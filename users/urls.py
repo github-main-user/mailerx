38d973d142142e01dc0f62c90c92336c61fa8487
github-main-user <kolya.skripn@mail.ru>
2025-06-02 21:52:14 +0300

    feat: add profile page for user

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from .apps import UsersConfig
-from .views import RegisterView
+from .views import ProfileView, RegisterView
 
 app_name = UsersConfig.name
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
     path("register/", RegisterView.as_view(), name="register"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
+from typing import override
+
 from django.contrib.auth import get_user_model, login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import FormView
+from django.views.generic import FormView, UpdateView
+
+from .forms import ProfileForm
 
 User = get_user_model()
 
@@ -9,9 +14,19 @@ User = get_user_model()
 class RegisterView(FormView):
     template_name = "users/register.html"
     form_class = UserCreationForm
-    success_url = reverse_lazy("mailings:home")
+    success_url = reverse_lazy("dashboard:home")
 
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
         return super().form_valid(form)
+
+
+class ProfileView(LoginRequiredMixin, UpdateView):
+    template_name = "users/profile.html"
+    form_class = ProfileForm
+    success_url = reverse_lazy("dashboard:home")
+
+    @override
+    def get_object(self, queryset=None):
+        return self.request.user
