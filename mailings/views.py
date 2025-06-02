b4f015cb992dc0f66b79699636d3685b06400604
github-main-user <kolya.skripn@mail.ru>
2025-06-02 16:35:21 +0300

    feat: display statistics on home page

--- mailings/views.py
+++ mailings/views.py
@@ -1,5 +1,32 @@
 from django.views.generic import TemplateView
 
+from clients.models import Client
+
+from .models import Mailing
+
 
 class HomeView(TemplateView):
     template_name = "mailings/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated:
+            mailings = Mailing.objects.filter(owner=self.request.user)  # type: ignore
+
+            # clients **for current user**
+            # since there is the (owner + email) unique constraint
+            # each received client is unique
+            clients = Client.objects.filter(owner=self.request.user)  # type: ignore
+
+            context.update(
+                {
+                    "total_mailings": mailings.count(),
+                    "active_mailings": mailings.filter(
+                        status=Mailing.MailingStatus.STARTED
+                    ).count(),
+                    "unique_clients": clients.count(),
+                }
+            )
+
+        return context
