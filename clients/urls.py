84d01aae0267169bac752a2b4547ada7a430f7f7
github-main-user <kolya.skripn@mail.ru>
2025-06-02 17:03:39 +0300

    feat: add missing CRUD for clients

--- clients/urls.py
+++ clients/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from .apps import ClientsConfig
-from .views import ClientCreateView, ClientListView
+from .views import ClientCreateView, ClientDeleteView, ClientListView, ClientUpdateView
 
 app_name = ClientsConfig.name
 
 urlpatterns = [
     path("", ClientListView.as_view(), name="client_list"),
     path("create/", ClientCreateView.as_view(), name="client_create"),
+    path("<int:pk>/update/", ClientUpdateView.as_view(), name="client_update"),
+    path("<int:pk>/delete/", ClientDeleteView.as_view(), name="client_delete"),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
 from .forms import ClientForm
 from .models import Client
@@ -7,8 +8,26 @@ from .models import Client
 
 class ClientListView(LoginRequiredMixin, ListView):
     model = Client
+    success_url = reverse_lazy("clients:client_list")
 
 
 class ClientCreateView(LoginRequiredMixin, CreateView):
     model = Client
     form_class = ClientForm
+    success_url = reverse_lazy("clients:client_list")
+
+    def form_valid(self, form):
+        client = form.instance
+        client.owner = self.request.user
+        return super().form_valid(form)
+
+
+class ClientUpdateView(LoginRequiredMixin, UpdateView):
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy("clients:client_list")
+
+
+class ClientDeleteView(LoginRequiredMixin, DeleteView):
+    model = Client
+    success_url = reverse_lazy("clients:client_list")
