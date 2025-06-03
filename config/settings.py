9c6259de51a21f4f8fcb3cdb2de96036e0103637
github-main-user <kolya.skripn@mail.ru>
2025-06-03 21:11:27 +0300

    feat: add custom user model

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
     "clients",
     "mail_messages",
     "mailings",
@@ -98,6 +99,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "users.User"
 
 
 # Internationalization
--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    country = models.CharField(max_length=30, null=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
