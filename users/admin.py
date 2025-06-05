29888b4abbdc8575e5766f94d7c1abf817eae745
github-main-user <kolya.skripn@mail.ru>
2025-06-05 17:31:52 +0300

    feat: include User model to admin page

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "email",
+        "first_name",
+        "last_name",
+        "phone_number",
+        "country",
+        "is_active",
+    ]
+    list_filter = ["country", "is_active"]
+    search_fields = ["first_name", "last_name"]
