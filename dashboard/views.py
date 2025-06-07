9ab5b87a66b4ff15e960e03e0d3b84332447e654
github-main-user <kolya.skripn@mail.ru>
2025-06-08 00:17:26 +0300

    fix: managers dont need statistics

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core.cache import cache
 from django.db.models import Count, Q
 from django.views.generic import TemplateView
@@ -6,6 +7,8 @@ from django.views.generic import TemplateView
 from clients.models import Client
 from mail_messages.models import Message
 from mailings.models import Mailing, MailingAttempt
+
+User = get_user_model()
 
 
 class HomeView(TemplateView):
@@ -15,7 +18,8 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         user = self.request.user
 
-        if user.is_authenticated:
+        # managers can't create/update/delete so they don't need statistics
+        if user.is_authenticated and user.role == User.UserRole.USER:
             cache_key = f"home_view_data_{user.pk}"
             cached_data = cache.get(cache_key)
 
