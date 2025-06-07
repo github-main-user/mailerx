daaa4b91c1878b9a58a30f2bd15a4995f1875108
github-main-user <kolya.skripn@mail.ru>
2025-06-07 13:27:16 +0300

    feat: use aggregation instead of multiple queryset evaluation

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,7 @@
+from django.conf import settings
+from django.db.models import Count, Q
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django.views.generic import TemplateView
 
 from clients.models import Client
@@ -10,31 +14,32 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        user = self.request.user
 
-        if self.request.user.is_authenticated:
-            mailings = Mailing.objects.filter(owner=self.request.user)  # type: ignore
-            mail_messages = Message.objects.filter(owner=self.request.user)  # type: ignore
-            attempts = MailingAttempt.objects.filter(mailing__owner=self.request.user)  # type: ignore
+        if user.is_authenticated:
+            clients = Client.objects.filter(owner=user)
+            mail_messages = Message.objects.filter(owner=user)
 
-            # clients **for current user**
-            # since there is the (owner + email) unique constraint
-            # each received client is unique
-            clients = Client.objects.filter(owner=self.request.user)  # type: ignore
+            mailings = Mailing.objects.filter(owner=user).aggregate(
+                total=Count("pk"),
+                active=Count("pk", filter=Q(status=Mailing.MailingStatus.STARTED)),
+            )
+
+            attempts = MailingAttempt.objects.filter(mailing__owner=user).aggregate(
+                success=Count(
+                    "pk", filter=Q(status=MailingAttempt.AttemptStatus.SUCCESS)
+                ),
+                fail=Count("pk", filter=Q(status=MailingAttempt.AttemptStatus.FAIL)),
+            )
 
             context.update(
                 {
-                    "total_mailings": mailings.count(),
-                    "active_mailings": mailings.filter(
-                        status=Mailing.MailingStatus.STARTED
-                    ).count(),
-                    "unique_clients": clients.count(),
+                    "total_mailings": mailings["total"],
+                    "active_mailings": mailings["active"],
+                    "total_clients": clients.count(),
                     "total_messages": mail_messages.count(),
-                    "successfull_attempts": attempts.filter(
-                        status=MailingAttempt.AttemptStatus.SUCCESS
-                    ).count(),
-                    "failed_attempts": attempts.filter(
-                        status=MailingAttempt.AttemptStatus.FAIL
-                    ).count(),
+                    "successfull_attempts": attempts["success"],
+                    "failed_attempts": attempts["fail"],
                 }
             )
 
